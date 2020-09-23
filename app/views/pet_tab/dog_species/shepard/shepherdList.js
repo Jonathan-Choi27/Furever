9303bbb074f6efa36d0a963b74b7678e646bcc99
Jonathan Choi <mcho2285@uni.sydney.edu.au>
2020-09-24 01:21:02 +1000

    Added cardlist template

--- app/views/pet_tab/dog_species/shepard/shepherdList.js
+++ app/views/pet_tab/dog_species/shepard/shepherdList.js
@@ -20,24 +20,28 @@ import {
   Icon,
   SearchBar,
   Image,
+  Avatar,
 } from "react-native-elements";
 import { db } from "../../../database/firebase";
 import { auth } from "../../../database/firebase";
 export default class shepherdList extends React.Component {
   state = {
-    listings: [],
+    data: [],
   };
+
   render() {
+    const array = [];
     db.collection("pet-sell-list")
       .get()
       .then((snapshot) => {
-        snapshot.docs.forEach((user) => {
-          this.state.listings.push({
-            id: "1121212",
-            title: user.data().name,
-          });
+        snapshot.docs.some((user) => {
+          array.push({ title: user.data().name });
+        });
+        this.setState({
+          data: [...array],
         });
       });
+
     return (
       <View style={styles.container}>
         <View style={styles.buySellContainer}>
@@ -81,9 +85,9 @@ export default class shepherdList extends React.Component {
         </View>
         <Text style={styles.outsideTitle}>German Shepherd</Text>
         <FlatList
-          data={this.state.listings}
           renderItem={({ item }) => <Item title={item.title} />}
           keyExtractor={(item, index) => index.toString()}
+          data={this.state.data}
           extraData={this.state}
         />
       </View>
@@ -92,31 +96,40 @@ export default class shepherdList extends React.Component {
 }
 
 const Item = ({ title }) => (
-  <Card>
-    <Card.Title>{title}</Card.Title>
-    <Card.Divider />
-    <Card.Image
-      source={{
-        uri:
-          "https://firebasestorage.googleapis.com/v0/b/pet-search-soft3888.appspot.com/o/images%2Flogo.svg?alt=media&token=21d331fe-dc33-4021-a632-aeaa3b7cf6c4",
-      }}
-    >
-      <Text style={{ marginBottom: 10 }}>
-        The idea with React Native Elements is more about component structure
-        than actual design.
-      </Text>
-      <Button
-        icon={<Icon name="code" color="#ffffff" />}
-        buttonStyle={{
-          borderRadius: 0,
-          marginLeft: 0,
-          marginRight: 0,
-          marginBottom: 0,
+  <Card
+    containerStyle={styles.card}
+    title={title}
+    image={
+      "https://randomwordgenerator.com/img/picture-generator/54e6dc454e55b10ff3d8992cc12c30771037dbf85254784a73287bd09344_640.jpg"
+    }
+  >
+    <View style={styles.avatarContainer}>
+      <Avatar
+        rounded
+        source={{
+          uri:
+            "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
         }}
-        title="More info"
-        onPress={() => console.log(DATA)}
       />
-    </Card.Image>
+      <Text>Sample Name</Text>
+    </View>
+
+    <Text style={{ marginBottom: 10 }}>
+      The idea with React Native Elements is more about component structure than
+      actual design.
+    </Text>
+    <Button
+      icon={{ name: "code" }}
+      backgroundColor="#03A9F4"
+      fontFamily="Lato"
+      buttonStyle={{
+        borderRadius: 0,
+        marginLeft: 0,
+        marginRight: 0,
+        marginBottom: 0,
+      }}
+      title="VIEW NOW"
+    />
   </Card>
 );
 
@@ -168,5 +181,11 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     fontWeight: "bold",
     padding: 12,
+  },
+  card: {
+    borderRadius: 10,
+  },
+  avatarContainer: {
+    fontWeight: "bold",
   },
 });
