527b72a307fb946e4f5da763c29809f6611336d9
Uyen Tran <utra4729@uni.sydney.edu.au>
2020-09-26 15:37:47 +1000

    changed view to cards

--- app/views/pet_tab/petSellProfile.js
+++ app/views/pet_tab/petSellProfile.js
@@ -13,6 +13,14 @@ import {
   Dimensions,
   Image,
 } from "react-native";
+import {
+  Card,
+  ListItem,
+  Button,
+  Icon,
+  SearchBar,
+  Avatar,
+} from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { db } from "../database/firebase";
 
@@ -23,7 +31,7 @@ export default class petSellProfile extends React.Component {
 
   componentDidMount() {
     const dataArray = [];
-    db.collection("pet-sell-list")
+    db.collection("pet_listings")
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((listingDoc) => {
@@ -74,58 +82,62 @@ export default class petSellProfile extends React.Component {
             <Text style={styles.fontHeading}> General Information </Text>
           </View>
 
-          <View style={styles.boxContainer}>
-            <View>
-              <View style={styles.imageContainer}></View>
-              <Text style={{ textAlign: "center", paddingTop: 5 }}>
-                <Text style={{ fontWeight: "bold" }}>Price:</Text>{" "}
-                <Text>$7,000</Text>
-              </Text>
+          <Card containerStyle={styles.cardContainer}>
+            <View style={styles.cardContentContainer}>
+              <View>
+                <View style={styles.imageContainer}></View>
+                <Text style={{ textAlign: "center", paddingTop: 5 }}>
+                  <Text style={{ fontWeight: "bold" }}>Price:</Text>{" "}
+                  <Text>$7,000</Text>
+                </Text>
+              </View>
+              <View style={{ paddingLeft: 15 }}>
+                <Text>
+                  <Text style={{ fontWeight: "bold" }}>Name:</Text>{" "}
+                  <Text>Marshmallow</Text>
+                </Text>
+                <Text>
+                  <Text style={{ fontWeight: "bold" }}>Category:</Text>{" "}
+                  <Text>Dog</Text>
+                </Text>
+                <Text>
+                  <Text style={{ fontWeight: "bold" }}>Breed:</Text>{" "}
+                  <Text>Samoyed</Text>
+                </Text>
+                <Text>
+                  <Text style={{ fontWeight: "bold" }}>Colour:</Text>{" "}
+                  <Text>White</Text>
+                </Text>
+                <Text>
+                  <Text style={{ fontWeight: "bold" }}>Age:</Text>{" "}
+                  <Text>3 months</Text>
+                </Text>
+                <Text>
+                  <Text style={{ fontWeight: "bold" }}>Gender:</Text>{" "}
+                  <Text>Female</Text>
+                </Text>
+                <Text>
+                  <Text style={{ fontWeight: "bold" }}>Size:</Text>{" "}
+                  <Text>Small</Text>
+                </Text>
+                <Text>
+                  <Text style={{ fontWeight: "bold" }}>Location:</Text>{" "}
+                  <Text>Sydney, NSW</Text>
+                </Text>
+              </View>
             </View>
-            <View style={{ paddingLeft: 15 }}>
-              <Text>
-                <Text style={{ fontWeight: "bold" }}>Name:</Text>{" "}
-                <Text>Marshmallow</Text>
-              </Text>
-              <Text>
-                <Text style={{ fontWeight: "bold" }}>Category:</Text>{" "}
-                <Text>Dog</Text>
-              </Text>
-              <Text>
-                <Text style={{ fontWeight: "bold" }}>Breed:</Text>{" "}
-                <Text>Samoyed</Text>
-              </Text>
-              <Text>
-                <Text style={{ fontWeight: "bold" }}>Colour:</Text>{" "}
-                <Text>White</Text>
-              </Text>
-              <Text>
-                <Text style={{ fontWeight: "bold" }}>Age:</Text>{" "}
-                <Text>3 months</Text>
-              </Text>
-              <Text>
-                <Text style={{ fontWeight: "bold" }}>Gender:</Text>{" "}
-                <Text>Female</Text>
-              </Text>
-              <Text>
-                <Text style={{ fontWeight: "bold" }}>Size:</Text>{" "}
-                <Text>Small</Text>
-              </Text>
-              <Text>
-                <Text style={{ fontWeight: "bold" }}>Location:</Text>{" "}
-                <Text>Sydney, NSW</Text>
-              </Text>
-            </View>
-          </View>
+          </Card>
 
-          <View style={styles.boxContainer}>
-            <Text style={styles.fontHeading}> General Information </Text>
-            <FlatList
-              data={this.state.data}
-              keyExtractor={(item, index) => index.toString()}
-              renderItem={({ item }) => <Text>{item.title}</Text>}
-            />
-          </View>
+          <Card containerStyle={styles.cardContainer}>
+            <View style={styles.boxContainer}>
+              <Text style={styles.fontHeading}>General Information </Text>
+              <FlatList
+                data={this.state.data}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => <Text>{item.title}</Text>}
+              />
+            </View>
+          </Card>
 
           <View style={{ height: 50, paddingBottom: 20 }}>
             <TouchableOpacity
@@ -156,29 +168,14 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     alignSelf: "stretch",
-    padding: 20,
+    paddingTop: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
   },
   imageContainer: {
     width: 150,
     height: 150,
     backgroundColor: "pink",
-  },
-  boxContainer: {
-    alignSelf: "stretch",
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-    flexDirection: "row",
-    backgroundColor: "white",
-    height: 200,
-    marginLeft: 20,
-    marginRight: 20,
-    marginBottom: 20,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-    elevation: 1,
-    padding: 15,
   },
   categories: {
     alignSelf: "stretch",
@@ -205,9 +202,24 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   fontHeading: {
-    paddingTop: 10,
     textAlign: "left",
     fontSize: 16,
     fontWeight: "bold",
+  },
+  cardContainer: {
+    borderRadius: 4,
+    alignSelf: "stretch",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    flexDirection: "row",
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  cardContentContainer: {
+    borderRadius: 4,
+    alignSelf: "stretch",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    flexDirection: "row",
   },
 });
