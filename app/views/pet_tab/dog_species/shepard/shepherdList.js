c88132274088c10bce840ccef327dccd5b04ca37
Jonathan Choi <mcho2285@uni.sydney.edu.au>
2020-09-27 17:13:30 +1000

    Fixed Home & ShepherdList CSS, Added Search Func

--- app/views/pet_tab/dog_species/shepard/shepherdList.js
+++ app/views/pet_tab/dog_species/shepard/shepherdList.js
@@ -6,11 +6,20 @@ import {
   TouchableOpacity,
   FlatList,
 } from "react-native";
-import { Avatar, Card, Button, Searchbar } from "react-native-paper";
+import {
+  Avatar,
+  ActivityIndicator,
+  Card,
+  Button,
+  Searchbar,
+} from "react-native-paper";
 import { db } from "../../../database/firebase";
 export default class shepherdList extends React.Component {
   state = {
     data: [],
+    isLoading: true,
+    filteredData: [],
+    searchText: "",
   };
 
   async componentDidMount() {
@@ -37,12 +46,29 @@ export default class shepherdList extends React.Component {
             location: listingDoc.data().location,
             gender: listingDoc.data().gender,
           });
-          this.setState({ data: [...dataArray] });
+          this.setState({ isLoading: false, data: [...dataArray] });
         });
       });
   }
 
+  searchFunction = (searchText) => {
+    this.setState({ searchText: searchText });
+
+    let filteredData = this.state.data.filter(function (item) {
+      return item.title.includes(searchText);
+    });
+
+    this.setState({ filteredData: filteredData });
+  };
+
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.activityContainer}>
+          <ActivityIndicator size="large" color="#447ECB" />
+        </View>
+      );
+    }
     return (
       <View style={styles.container}>
         <View style={styles.buySellContainer}>
@@ -71,57 +97,56 @@ export default class shepherdList extends React.Component {
             <Text style={{ textAlign: "center" }}> Sell </Text>
           </TouchableOpacity>
         </View>
-        <View>
-          <Searchbar
-            style={{
-              flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-              height: 30,
-              width: 350,
-              margin: 10,
-            }}
-            placeholder="Search..."
-          />
-          <View style={styles.titleContainer}>
-            <Text style={styles.title}>German Shepherd</Text>
-            <Button color="#447ECB" onPress={() => {}} mode="contained">
-              Information
-            </Button>
-          </View>
-        </View>
-        <FlatList
-          showsVerticalScrollIndicator={false}
-          renderItem={({ item }) => (
-            <Card style={styles.card}>
-              <Card.Cover source={item.photo} />
-              <Card.Title
-                title={item.title}
-                subtitle={item.name}
-                left={(props) => (
-                  <Avatar.Image {...props} size={40} source={item.photo} />
-                )}
-              />
-              <Card.Content>
-                <Text style={styles.cardContentText}>Age: {item.age}</Text>
-                <Text style={styles.cardContentText}>
-                  Gender: {item.gender}
-                </Text>
-                <Text style={styles.cardContentText}>
-                  Location: {item.location}
-                </Text>
-              </Card.Content>
-              <Card.Actions>
-                <Button color="#447ECB" onPress={() => {}}>
-                  More info
-                </Button>
-              </Card.Actions>
-            </Card>
-          )}
-          keyExtractor={(item, index) => index.toString()}
-          data={this.state.data}
-          extraData={this.state}
+        <Searchbar
+          style={styles.searchBar}
+          placeholder="Search"
+          onChangeText={this.searchFunction}
+          value={this.state.searchText}
         />
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>German Shepherd</Text>
+          <Button color="#447ECB" onPress={() => {}} mode="contained">
+            Information
+          </Button>
+        </View>
+        <View style={styles.cardContainer}>
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <Card elevation={5} style={styles.card}>
+                <Card.Cover source={item.photo} />
+                <Card.Title
+                  title={item.title}
+                  subtitle={item.name}
+                  left={(props) => (
+                    <Avatar.Image {...props} size={40} source={item.photo} />
+                  )}
+                />
+                <Card.Content>
+                  <Text style={styles.cardContentText}>Age: {item.age}</Text>
+                  <Text style={styles.cardContentText}>
+                    Gender: {item.gender}
+                  </Text>
+                  <Text style={styles.cardContentText}>
+                    Location: {item.location}
+                  </Text>
+                </Card.Content>
+                <Card.Actions>
+                  <Button color="#447ECB" onPress={() => {}}>
+                    More info
+                  </Button>
+                </Card.Actions>
+              </Card>
+            )}
+            keyExtractor={(item, index) => index.toString()}
+            data={
+              this.state.filteredData && this.state.filteredData.length > 0
+                ? this.state.filteredData
+                : this.state.data
+            }
+            //extraData={this.state}
+          />
+        </View>
       </View>
     );
   }
@@ -133,20 +158,36 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  activityContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   buySellContainer: {
     alignSelf: "stretch",
     justifyContent: "flex-start",
     alignItems: "flex-start",
     flexDirection: "row",
   },
+  searchBar: {
+    height: 50,
+    width: 350,
+    margin: 10,
+  },
   titleContainer: {
     flexDirection: "row",
-    justifyContent: "space-between",
-    margin: 12,
+    justifyContent: "flex-start",
+    padding: 20,
   },
   title: {
     fontWeight: "bold",
     fontSize: 20,
+    marginRight: 25,
+  },
+  cardContainer: {
+    flex: 2,
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
   },
   card: {
     margin: 5,
