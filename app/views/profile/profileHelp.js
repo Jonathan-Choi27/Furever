83ed5488320fa4a8b8afd5939bccd0e1f697fb41
Jong Woong To <joto5719@uni.sydney.edu.au>
2020-11-09 19:35:29 +1100

    added missing import statement

--- app/views/profile/profileHelp.js
+++ app/views/profile/profileHelp.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Image, Text, View } from "react-native";
+import { Image, Text, View, BackHandler } from "react-native";
 import { Card } from "react-native-elements";
 import globalStyles from "../styleSheet/styleSheet";
 
